7d266bbc6536b2e1b49d4e479dff79b9a61203aa
oriionn <contact@oriondev.fr>
2024-03-04 23:54:39 +0100

    Ecriture d'un README
    Suppression d'une fonction déconseillé

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # ScolengoICALAPI
-A little API for sync your Scolengo's timetable to Google Calendar
+Une simple API qui vous permets de relier votre emploi du temps Skolengo à votre calendrier.
+
+## Installation
+### Standalone
+```bash
+git clone https://github.com/oriionn/ScolengoICALAPI.git
+cd ScolengoICALAPI
+npm install
+```
+
+### Docker
+```bash
+git clone 
+cd ScolengoICALAPI
+docker build -t scolengoicalapi .
+docker run -d -e APIKEY=YOUR_API_KEY -p 3000:3000 --name scolengoicalapi -v /path/to/the/data/folder:/usr/src/app/data scolengoicalapi
+```
+##### Remplacez `YOUR_API_KEY` par votre clé d'API, cela vous permettra de protéger votre emploi du temps.
+##### Remplacez `/path/to/the/data/folder` par le chemin absolu du dossier `data` dans lequel vous avez mis votre fichier `skolengo.json`.
+
+## Configuration
+1. Renommez le fichier `.env.example` en `.env` et modifiez les valeurs pour correspondre à votre configuration.
+2. Générez un fichier pour Skolengo en utilisant [scolengo-token](https://github.com/maelgangloff/scolengo-token) et le mettre dans le dossier `data` en le nommant `skolengo.json`.
+
+## Utilisation
+### API
+#### `GET /?key=YOUR_API_KEY`
+Renvoie le fichier iCal de votre emploi du temps.
+### Avec Google Calendar
+Pour utiliser le projet avec Google Calendar, vous pouvez utiliser ce [projet](https://github.com/derekantrican/GAS-ICS-Sync) qui permets de définir une intervalle de synchronisation pour les iCal.
+
+## License
+Le projet est sous [GPL-3.0](https://www.gnu.org/licenses/gpl-3.0.html).
--- app.js
+++ app.js
@@ -12,30 +12,29 @@ const config = JSON.parse(fs.readFileSync('data/skolengo.json'));
 require('dotenv').config();
 
 // Initialisation de Skolengo
-Skolengo.fromConfigObject(config).then(async user => {
-	async function refreshToken() {
-		config.tokenSet = await user.refreshToken(true);
-		fs.writeFileSync("data/skolengo.json", JSON.stringify(config));
-	}
-	setInterval(refreshToken, 1000 * 60 * 60 * 3)
-	refreshToken()
+Skolengo.fromConfigObject(config, {
+		onTokenRefresh: (tokenSet) => {
+			let newConfig = config;
+			newConfig.tokenSet = tokenSet;
+			fs.writeFileSync("data/skolengo.json", JSON.stringify(newConfig));
+		}
+	}).then(async user => {
+		let studentId = (await user.getUserInfo()).id;
 
-	let studentId = (await user.getUserInfo()).id;
+		fastify.get("/", async (request, reply) => {
+			if (request.query.key !== process.env.APIKEY) reply.send({ error: "Invalid API key" });
 
-	fastify.get("/", async (request, reply) => {
-		if (request.query.key !== process.env.APIKEY) reply.send({ error: "Invalid API key" });
+			let currentDate = new Date();
+			let futureDate = new Date();
+			futureDate.setDate(currentDate.getDate() + 14);
+			futureDate = futureDate.toISOString().split("T")[0];
+			currentDate = currentDate.toISOString().split("T")[0];
 
-		let currentDate = new Date();
-		let futureDate = new Date();
-		futureDate.setDate(currentDate.getDate() + 14);
-		futureDate = futureDate.toISOString().split("T")[0];
-		currentDate = currentDate.toISOString().split("T")[0];
+			reply.send((await user.getAgenda(studentId, currentDate, futureDate)).toICalendar());
+		});
 
-		reply.send((await user.getAgenda(studentId, currentDate, futureDate)).toICalendar());
-	});
-
-	fastify.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
-		if (err) throw err;
-		console.log(`Server listening on ${address}`)
-	});
-})
+		fastify.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
+			if (err) throw err;
+			console.log(`Server listening on ${address}`)
+		});
+	})
